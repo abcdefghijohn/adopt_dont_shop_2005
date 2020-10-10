6ab32663c39cdbdb2cc5c1b82e5506323e5daa99
John Kim <abcdefghijohnk@gmail.com>
2020-10-10 12:13:52 -0600

    migrate pets table, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_234553) do
+ActiveRecord::Schema.define(version: 2020_10_10_035943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "image"
+    t.string "name"
+    t.integer "approx_age"
+    t.string "sex"
+    t.string "shelter"
+    t.integer "shelter_id"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
