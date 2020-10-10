3f53df9d29437f0f08dae74fd4d9f88f4de80d19
John Kim <abcdefghijohnk@gmail.com>
2020-10-10 17:37:57 -0600

    update schema with pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_182624) do
+ActiveRecord::Schema.define(version: 2020_10_10_200929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,11 @@ ActiveRecord::Schema.define(version: 2020_10_10_182624) do
   create_table "pets", force: :cascade do |t|
     t.string "image"
     t.string "name"
-    t.integer "approx_age"
+    t.integer "age"
     t.string "sex"
-    t.integer "shelter_id"
+    t.string "shelter_name"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_10_10_182624) do
     t.integer "zip"
   end
 
+  add_foreign_key "pets", "shelters"
 end
