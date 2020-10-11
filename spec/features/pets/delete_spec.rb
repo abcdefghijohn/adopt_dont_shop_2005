4dbfa3f9f426db8dbbc1cf1e6e609cb4a9158099
John Kim <abcdefghijohnk@gmail.com>
2020-10-11 17:42:18 -0600

    edit create pet test

--- spec/features/pets/delete_spec.rb
+++ spec/features/pets/delete_spec.rb
@@ -11,14 +11,14 @@ require 'rails_helper'
 describe "As a visitor when I visit the pet show page" do
   describe "Then I see a link 'Delete Pet'" do
     it "When I click the link the pet is deleted and I am redirected to the pet index" do
-      pet_1 = shelter_1.pets.create(image: "https://images.dog.ceo/breeds/poodle-toy/n02113624_1832.jpg",
+      pet_1 = Pet.create(image: "https://images.dog.ceo/breeds/poodle-toy/n02113624_1832.jpg",
                                  name: "Jim",
                                  description: "Friendly",
                                  age: 4,
                                  sex: "M",
                                  adoption_status: 'Adoptable'
                                 )
-      pet_1 = shelter_1.pets.create(image: "https://images.dog.ceo/breeds/poodle-toy/n02113624_1832.jpg",
+      pet_1 = Pet.create(image: "https://images.dog.ceo/breeds/poodle-toy/n02113624_1832.jpg",
                                  name: "Jim",
                                  description: "Friendly",
                                  age: 4,
@@ -26,7 +26,7 @@ describe "As a visitor when I visit the pet show page" do
                                  adoption_status: 'Adoptable'
                                 )
       visit "/pets/#{pet_1.id}"
-      click_link 'Delete Pet'
+      click_button 'Delete Pet'
       save_and_open_page
       expect(page).to_not have_content(pet_1.name)
     end
