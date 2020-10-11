52c0bda817f1f8d19814818e33bff557976878c8
John Kim <abcdefghijohnk@gmail.com>
2020-10-11 15:55:05 -0600

    remove shelter_name from model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
-  validates_presence_of :image, :name, :description, :age, :sex, :shelter_name, :adoption_status
+  validates_presence_of :image, :name, :description, :age, :sex, :adoption_status
 
 end
