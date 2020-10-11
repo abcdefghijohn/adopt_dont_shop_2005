025ec7ed68cc8cfe77b20f0cb8bdd2dfcce600ec
John Kim <abcdefghijohnk@gmail.com>
2020-10-11 16:34:26 -0600

    create update behavior for pet controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,6 +23,18 @@ class PetsController < ApplicationController
     redirect_to "/shelters/#{pet.shelter_id}/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update(pet_params)
+
+    pet.save
+    redirect_to "/pets/#{pet.id}"
+  end
+
 
   def pet_params
     {
