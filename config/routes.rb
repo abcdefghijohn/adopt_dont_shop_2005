be810637356d10b502cfad51cb825229cafee75e
John Kim <abcdefghijohnk@gmail.com>
2020-10-11 15:55:39 -0600

    change route for pet new

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+
   get '/pets', to: 'pets#index'
   get '/shelters/:shelter_id/pets/new', to: 'pets#new'
-  post '/shelters/:shelter_id/pets', to: 'pets#create'
-  get '/shelters/:id/pets', to: 'pets#show'
+  post '/pets', to: 'pets#create'
+  get '/shelters/:id/pets', to: 'pets#index_shelter'
+  get '/pets/:id', to: 'pets#show'
 end
